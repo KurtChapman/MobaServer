91318a63c04a97cdfb5994b263cd1ffe00470683
Unknown <kurty88@hotmail.co.uk>
2017-04-10 19:02:37 +0100

    Server Connection up and running

---
+++ MobaServer/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using MobaServer.Transport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobaServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SocketHandler socket = new SocketHandler();
+
+            while (true)
+            {
+            }
+        }
+    }
+}
---
+++ MobaServer/ConsoleApp1/Transport/SocketHandler.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace MobaServer.Transport
+{
+    public class SocketState
+    {
+        public Socket clientSocket = null;
+        public const int READ_BUFFER_SIZE = 1024;
+        public byte[] buffer = new byte[READ_BUFFER_SIZE];
+    }
+
+    class SocketHandler
+    {
+        private static ManualResetEvent operationComplete = new ManualResetEvent(false);
+
+
+        public SocketHandler()
+        {
+            BeginListening();
+        }
+
+        private void BeginListening()
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            socket.Bind(new IPEndPoint(IPAddress.Any, 8080));
+            socket.Listen(128);
+            
+
+            while (true)
+            {
+                // Set the event to nonsignaled state.  
+                operationComplete.Reset();
+
+                // Start an asynchronous socket to listen for connections.  
+                Console.WriteLine("Waiting for a connection...");
+                socket.BeginAccept(null, 0, new AsyncCallback(OnAccept), socket);
+
+                // Wait until a connection is made before continuing.  
+                operationComplete.WaitOne();
+            }
+        }
+
+        private static void OnRead(IAsyncResult result)
+        {
+            String content = String.Empty;
+
+            // Retrieve the state object and the handler socket  
+            // from the asynchronous state object.  
+            SocketState state = (SocketState)result.AsyncState;
+            Socket handler = state.clientSocket;
+
+            // Read data from the client socket.   
+            int bytesRead = handler.EndReceive(result);
+            string sent = Encoding.ASCII.GetString(state.buffer, 0, bytesRead);
+            Console.WriteLine(sent);
+
+
+            if (bytesRead > 0)
+            {
+                //handle the stuff
+            }
+
+            handler.BeginReceive(state.buffer, 0, SocketState.READ_BUFFER_SIZE, 0, new AsyncCallback(OnRead), state);
+        }
+
+        private static void OnAccept(IAsyncResult result)
+        {
+            // Signal the main thread to continue.  
+            operationComplete.Set();
+
+            // Get the socket that handles the client request.  
+            Socket listener = (Socket)result.AsyncState;
+            Socket handler = listener.EndAccept(result);
+
+            // Create the state object.  
+            Console.WriteLine("Opened New Socket");
+            SocketState state = new SocketState();
+            state.clientSocket = handler;
+            handler.BeginReceive(state.buffer, 0, SocketState.READ_BUFFER_SIZE, 0, new AsyncCallback(OnRead), state);
+        }
+    }
+}
